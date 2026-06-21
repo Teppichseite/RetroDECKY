6e3d315fb63c150da2fffe1976bff73d3da3faa2
Teppichseite <teppichop@yahoo.de>
2026-06-21 12:20:47 +0200

    Updated license

--- src/components/shared.tsx
+++ src/components/shared.tsx
@@ -1,4 +1,10 @@
 // Taken from https://github.com/Teppichseite/DeckPass/blob/main/src/components/shared.tsx
+//
+// Copyright (C) 2025 Teppichseite
+// Licensed under the GNU General Public License v3.0 or (at your option) any later version.
+//
+// Modifications Copyright (C) 2025-2026 Teppichseite
+// Licensed under the GNU General Public License v3.0 or (at your option) any later version.
 
 export interface ButtonItemIconContentProps {
   children: React.ReactNode;
